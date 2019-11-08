f2a167e062ab90cbdccbc0ef4b166639030a732a
bolo.shi <bolo.shi@tyj365.com>
2019-11-08 16:29:20 +0800

    commit

--- multiThread/src/main/java/Main/BiliSpiderMain.java
+++ multiThread/src/main/java/Main/BiliSpiderMain.java
@@ -1,14 +1,15 @@
 package Main;
 
-import Spider.BiliSpider;
-import common.MybatisUtil;
-import org.apache.ibatis.io.Resources;
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -22,27 +23,158 @@ import java.util.Map;
  */
 public class BiliSpiderMain {
     public static Integer threadCount = 0;
-    public static void main(String[] args) throws InterruptedException {
-        Map<String, Integer> aimUrls = new HashMap<>();
-        aimUrls.put("https://api.bilibili.com/pgc/season/index/result?season_type=1&pagesize=20&type=1&page=", 146);
-        aimUrls.put("https://api.bilibili.com/pgc/season/index/result?season_type=2&pagesize=20&type=1&page=", 69);
-        aimUrls.put("https://api.bilibili.com/pgc/season/index/result?season_type=3&pagesize=20&type=1&page=", 146);
-        aimUrls.put("https://api.bilibili.com/pgc/season/index/result?season_type=4&pagesize=20&type=1&page=", 29);
-        aimUrls.put("https://api.bilibili.com/pgc/season/index/result?season_type=5&pagesize=20&type=1&page=", 124);
+    private static List<String> mediaIds = new ArrayList<>();
+    private static List<Map<String, Object>> data = new ArrayList<>();
 
-        for (Map.Entry<String, Integer> entry : aimUrls.entrySet()) {
-            BiliSpider biliSpider = new BiliSpider(entry.getKey(), entry.getValue());
-            Thread thread = new Thread(biliSpider);
-            thread.start();
-            synchronized (threadCount) {
-                threadCount++;
+    public static void main(String[] args) throws InterruptedException {
+        List<String> aimUrls = new ArrayList<>();
+        //cartoon mediaId list
+        aimUrls.add("https://api.bilibili.com/pgc/season/index/result?season_type=1&pagesize=20&type=1&page=");
+        //movie mediaId list
+        aimUrls.add("https://api.bilibili.com/pgc/season/index/result?season_type=2&pagesize=20&type=1&page=");
+        //documentaty mediaId list
+        aimUrls.add("https://api.bilibili.com/pgc/season/index/result?season_type=3&pagesize=20&type=1&page=");
+        //demestic mediaId list
+        aimUrls.add("https://api.bilibili.com/pgc/season/index/result?season_type=4&pagesize=20&type=1&page=");
+        //TV drama mediaId list
+        aimUrls.add("https://api.bilibili.com/pgc/season/index/result?season_type=5&pagesize=20&type=1&page=");
+
+
+
+        //获取全部mediaID
+        for (String url : aimUrls) {
+            new Thread(() -> {
+                //启动一个新线程开始获取mediaId
+                synchronized (threadCount) {
+                    threadCount++;
+                    System.out.println(Thread.currentThread().getName() + "启动");
+                }
+                List<String> tempMID = new ArrayList<>();
+                StringBuffer resources = new StringBuffer();
+                int page = 1;
+                StringBuffer stringBuffer = new StringBuffer(url);
+                JSONArray list;
+                JSONObject data;
+                for (; ; ) {
+                    stringBuffer.append(page);
+                    String response = connectGetRes(stringBuffer.toString());
+                    data = JSONObject.parseObject(response).getJSONObject("data");
+                    list = data.getJSONArray("list");
+                    for (Object object : list) {
+                        JSONObject info = (JSONObject) object;
+                        tempMID.add(info.get("media_id").toString());
+                    }
+                    if (data.get("has_next").toString().equals("0")) {
+                        stringBuffer.delete(87, stringBuffer.length());
+                        resources.setLength(0);
+                        page += 1;
+                    } else {
+                        synchronized (threadCount) {
+                            System.out.println(Thread.currentThread().getName() + stringBuffer.toString() + "结束");
+                            System.out.println("mediaID total：  " + tempMID.size());
+                            mediaIds.addAll(tempMID);
+                            threadCount--;
+                        }
+                        break;
+                    }
+                }
+            }
+            ).start();
+        }
+        while (true) {
+            Thread.sleep(1000);
+            if (threadCount == 0) {
+                System.out.println(mediaIds.size());
+                break;
             }
         }
-        while (threadCount != 0) {
-            System.out.println("subthread hasn't finish");
-            Thread.sleep(1000);
+
+        //获取详细信息
+        int fromIndex = 0;
+        int toIndex=200;
+        boolean end =false;
+        while (!end) {
+            if (toIndex>=mediaIds.size()){
+                toIndex=mediaIds.size();
+                end = true;
+            }
+            //每200个mediaID开启一个线程爬取
+            List<String> subMediaIds = mediaIds.subList(fromIndex,toIndex);
+            new Thread(() -> {
+                StringBuffer infoUrl = new StringBuffer("https://api.bilibili.com/pgc/view/web/media?media_id=");
+                List<Map<String, Object>> tempData = new ArrayList<>();
+                synchronized (threadCount) {
+                    threadCount++;
+                    System.out.println(Thread.currentThread().getName() + "启动"+subMediaIds.size());
+                }
+                for (String mediaId : subMediaIds) {
+                    String response = connectGetRes(infoUrl.append(mediaId).toString());
+                    System.out.println(response);
+                    tempData.add(getVideoDetail(response));
+                    infoUrl.delete(53, infoUrl.length());
+                }
+                //爬取结束
+                synchronized (threadCount) {
+                    data.addAll(tempData);
+                    threadCount--;
+                    System.out.println(Thread.currentThread().getName() + "结束"+tempData.size());
+                }
+            }).start();
+            fromIndex=toIndex;
+            toIndex+=10;
         }
-        MybatisUtil.sqlSession.commit();
-        MybatisUtil.sqlSession.close();
+    }
+
+    public static String connectGetRes(String url) {
+        String response = "";
+        try {
+            URL url1 = new URL(url);
+            HttpURLConnection connection = (HttpURLConnection) url1.openConnection();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            response = bufferedReader.readLine();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return response;
+    }
+
+    public static Map<String, Object> getVideoDetail(String jsonString) {
+        Map<String, Object> map = new HashMap<>();
+        JSONObject result = JSONObject.parseObject(jsonString).getJSONObject("result");
+        JSONObject publish = result.getJSONObject("publish");
+        JSONArray jsonAreas = result.getJSONArray("areas");
+
+        Map<String, String> areas = new HashMap<>();
+        for (int i = 0; i < jsonAreas.size(); i++) {
+            JSONObject jsonObject = jsonAreas.getJSONObject(i);
+            areas.put(result.getString("media_id"), jsonObject.getString("id"));
+        }
+
+        JSONArray jsonStyles = result.getJSONArray("styles");
+        Map<String, String> styles = new HashMap<>();
+        for (int i = 0; i < jsonStyles.size(); i++) {
+            JSONObject jsonObject = jsonStyles.getJSONObject(i);
+            styles.put(result.getString("media_id"), jsonObject.getString("id"));
+        }
+//        map.put("mediaId",Integer.valueOf(result.getString("media_id")));
+        map.put("mediaId", result.getString("media_id"));
+        map.put("title", result.getString("title"));
+        map.put("actors", result.getString("actors"));
+        map.put("alias", result.getString("alias"));
+        map.put("cover", result.getString("cover"));
+        map.put("evaluate", result.getString("evaluate"));
+        map.put("originName", result.getString("origin_name"));
+//        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+//        Date date = sdf.parse(publish.getString("pub_date"));
+//        map.put("publishDate",date);
+        map.put("publishDate", publish.getString("pub_date"));
+        map.put("status", publish.getString("time_length_show"));
+        map.put("staff", result.getString("staff"));
+        map.put("type", result.getString("type"));
+        map.put("areas", areas);
+        map.put("styles", styles);
+        return map;
     }
 }
--- multiThread/src/main/java/Main/MultiThreadExercises.java
+++ multiThread/src/main/java/Main/MultiThreadExercises.java
@@ -1,5 +1,7 @@
 package Main;
 
+import java.io.*;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -14,25 +16,20 @@ import java.util.concurrent.atomic.AtomicReference;
  **/
 public class MultiThreadExercises {
         public static void main(String[] args) throws InterruptedException {
-//            AtomicReference<Integer> ref = new AtomicReference<>(new Integer(1000));
-//
-//            List<Thread> list = new ArrayList<>();
-//            for (int i = 0; i < 1000; i++) {
-//                Thread t = new Thread(new Task(ref), "Thread-" + i);
-//                list.add(t);
-//                t.start();
-//            }
-//
-//            for (Thread t : list) {
-//                t.join();
-//            }
-//
-//            System.out.println(ref.get());    // 打印2000
-            int a=1;
-            a = ~a;
-            System.out.println(a);
-            a = ~a;
-            System.out.println(~a);
+            AtomicReference<Integer> ref = new AtomicReference<>(new Integer(1000));
+
+            List<Thread> list = new ArrayList<>();
+            for (int i = 0; i < 1000; i++) {
+                Thread t = new Thread(new Task(ref), "Thread-" + i);
+                list.add(t);
+                t.start();
+            }
+
+            for (Thread t : list) {
+                t.join();
+            }
+
+            System.out.println(ref.get());    // 打印2000
         }
 
     }
@@ -53,5 +50,70 @@ public class MultiThreadExercises {
             }
         }
 }
+
+    class IoTest{
+        public static void main(String[] args) {
+            new Thread(()->{
+                InputStream is2 = null;
+                try {
+                    char bWrite[] = { 'a', '2', '3', '4', '6' };
+                    OutputStream os = new FileOutputStream("E:\\bolo\\test.txt");
+                    for (int x = 0; x < bWrite.length; x++) {
+                        os.write(bWrite[x]); // writes the bytes
+                        System.out.println("新线程休眠");
+                        Thread.sleep(2000);
+                    }
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+            try {
+                InputStream is = new FileInputStream("E:\\bolo\\test.txt");
+                int size = is.available();
+
+                for (int i = 0; i < size; i++) {
+                    System.out.print((char) is.read() + "  ");
+                    System.out.println("开始休眠");
+                    Thread.sleep(2000);
+                }
+            }catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    class NetTest{
+        public static void main(String[] args) throws InterruptedException {
+            Thread.sleep(5000);
+            for (int i =0;i<100;i++){
+                new Thread(()->{
+                    System.out.println(Thread.currentThread().getName()+"开始访问");
+                    for (int i1=0;i1<200;i1++) {
+                        try {
+                            URL url = new URL("http://112.74.174.246:8080/F_VideoManage/back/login/");
+                            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                            InputStream inputStream = connection.getInputStream();
+                            if (i1%10==0){
+                                System.out.println(Thread.currentThread().getName()+"访问"+i1+"次了");
+                                System.out.println(inputStream.read());
+                            }
+                        } catch (MalformedURLException e) {
+                            e.printStackTrace();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }).start();
+            }
+        }
+    }
 
 
